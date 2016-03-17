3181538f4892d5cac017f551e222f829e4a9413c
Subhajit Sahu <subhajit@codeaurora.org>
2016-03-17 18:14:42 +0530

    json: created json.stringify and it works great!

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,16 @@
 package main;
+import java.util.*;
+import java.util.function.*;
+import js.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
+			String[] a = new String[] {"hello", "java"};
+			Map b = new HashMap();
+			b.put("massage", a);
+			b.put("response", a);
+			System.out.println(JSON.stringify(b, (BiFunction)((Object k, Object v) -> { if(k.equals("message")) return "aaaaa"; return v; }), 4));
     }
 }
