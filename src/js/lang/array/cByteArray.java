276e4e8aad2f5634f4c226c2e28459cb89341c1f
Subhajit Sahu <wolfram77@gmail.com>
2016-04-23 22:23:25 +0530

    main: test unbound lambda from method

---
+++ src/js/lang/array/cByteArray.java
@@ -0,0 +1,20 @@
+package js.lang.array;
+import java.util.*;
+
+/**
+ *
+ * @author wolfram77
+ */
+public class cByteArray {
+	
+	/* data */
+	private final byte[] array;
+	
+	
+	/* constructor */
+
+	public cByteArray() {
+		this.array = null;
+	}
+	
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,21 @@
 package main;
-import java.nio.*;
+import java.util.function.*;
+import java.lang.reflect.*;
+import java.lang.invoke.*;
 
 public class Main {
 	
+	public static int add(int a) {
+		return a;
+	}
+	
 	public static void main(String[] args) throws Throwable {
-		ByteBuffer a = ByteBuffer.allocate(16);
+		MethodHandles.Lookup l = MethodHandles.lookup();
+		Method m = Main.class.getMethod("add", int.class);
+		MethodHandle mh = l.unreflect(m);
+		mh = LambdaMetafactory.metafactory(l, "apply", MethodType.methodType(BiFunction.class),
+			MethodType.genericMethodType(2), mh, mh.type()).getTarget();
+		BiFunction f = (BiFunction)mh.invoke();
+		System.out.println(f.apply(null, 1));
 	}
 }
