cde23629292f9b11cf38ba2b508e8ff3eec718b6
Subhajit Sahu <wolfram77@gmail.com>
2016-04-29 11:00:46 +0530

    function: iSub: created to contain common iSub* fields

---
+++ src/js/lang/function/iSub.java
@@ -0,0 +1,16 @@
+package js.lang.function;
+
+/**
+ * Represents a subroutine that can be called.
+ */
+public interface iSub extends iProc {
+	
+	/* static data */
+	/** Name of the method. */
+	static String NAME = "accept";
+	/** Array defining all iSub interfaces in order of input parameters */
+	static Class<?>[] CLASS = new Class<?>[] {
+		iSub0.class, iSub1.class, iSub2.class, iSub3.class,
+		iSub4.class, iSub5.class, iSub6.class, iSub7.class
+	};
+}
