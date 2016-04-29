a7a0cdf1de103a6dc32213116772d29936b6a948
Subhajit Sahu <wolfram77@gmail.com>
2016-04-29 11:49:49 +0530

    function: cMethodProc: updated to use iSub, iFn data

--- src/js/lang/function/cMethodProc.java
+++ src/js/lang/function/cMethodProc.java
@@ -7,20 +7,7 @@ import java.lang.invoke.*;
  * @param <TR> Return type of method.
  */
 public class cMethodProc<TR> implements iProc<TR> {
-	
-	/* static data */
-	/** Array defining all iSub interfaces in order of input parameters */
-	private static final Class<?>[] SUB_CLASS = new Class<?>[] {
-		iSub0.class, iSub1.class, iSub2.class, iSub3.class,
-		iSub4.class, iSub5.class, iSub6.class, iSub7.class
-	};
-	/** Array defining all iFn interfaces in order of input parameters */
-	private static final Class<?>[] FN_CLASS = new Class<?>[] {
-		iFn0.class, iFn1.class, iFn2.class, iFn3.class,
-		iFn4.class, iFn5.class, iFn6.class, iFn7.class
-	};
-	
-	
+
 	/* data */
 	/** Defines the method call procedure for the specified method. */
 	private final iProc proc;
@@ -45,9 +32,9 @@ public class cMethodProc<TR> implements iProc<TR> {
 			boolean stc = Modifier.isStatic(m.getModifiers());
 			boolean ret = m.getReturnType()==void.class;
 			int n = m.getParameterCount() + (!stc && obj==null? 1 : 0);
-			Class<?> dCls = ret? FN_CLASS[n] : SUB_CLASS[n];
+			Class<?> dCls = ret? iFn.CLASS[n] : iSub.CLASS[n];
 			MethodType dType = stc || obj==null? MethodType.methodType(dCls) : MethodType.methodType(dCls, m.getClass());
-			fctry = LambdaMetafactory.metafactory(l, ret? "apply" : "accept", dType, iProc.type(ret, n), mh, mh.type()).getTarget();
+			fctry = LambdaMetafactory.metafactory(l, ret? iFn.NAME : iSub.NAME, dType, iProc.type(ret, n), mh, mh.type()).getTarget();
 			proc = (iProc)(stc || obj==null? fctry.invoke() : fctry.invoke(obj));
 		}
 		catch(Throwable e) { throw new RuntimeException(e); }
