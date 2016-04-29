b6fae49df2c15ab5c0195b343db6ac5c83ed5b5e
Subhajit Sahu <wolfram77@gmail.com>
2016-04-29 13:45:41 +0530

    array: iList: renamed from iArray

--- src/js/lang/array/iList.java
+++ src/js/lang/array/iList.java
@@ -2,7 +2,7 @@ package js.lang.array;
 import js.lang.coll.map.iMap;
 import js.lang.object.*;
 
-public interface iArray<T> extends iMap<Integer, T> {
+public interface iList<T> extends iMap<Integer, T> {
 	
 	/* super method */
 	
